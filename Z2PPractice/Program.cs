83476ef2bbb4e80ef4c6d7bf068b1af6a0512c91
BlueLura <legogirl101@live.com>
2019-08-14 16:00:58 -0400

    Abstract classes tutorial

--- Z2PPractice/Program.cs
+++ Z2PPractice/Program.cs
@@ -6,9 +6,38 @@ namespace Z2PPractice
     {
         static void Main(string[] args)
         {
-
+            Dog dog = new Dog();
+            dog.Bark();
         }
     }
 
+    abstract class Animal
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
 
+        public void Breathe()
+        {
+            Console.WriteLine("I'm breathing.");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Nom nom.");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public int NumberOfLegs { get; set; }
+        public void Bark()
+        {
+            Console.WriteLine("Woof woof!");
+        }
+    }
+
+    class Fish : Animal
+    {
+
+    }
 }
